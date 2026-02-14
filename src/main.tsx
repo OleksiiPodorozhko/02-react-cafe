167e06aa7113b52e53e0e1af66095498b86c6f63
oleksiipodorozhko <oleksii.podorozhko@gmail.com>
2026-02-14 14:22:19 +0200

    Finish homework

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App.tsx';
+import App from './components/App/App.tsx';
 import 'modern-normalize';
-import './global.css';
-import TestComponent from './components/TestComponent.tsx';
+// import './global.css';
+// import TestComponent from './components/TestComponent.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <TestComponent />
     <App />
   </React.StrictMode>
 );
